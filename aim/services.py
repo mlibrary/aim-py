c37791e558943b348b47d8ef5062608bd1c5a669
Monique Rio <mrio@umich.edu>
2024-09-25 22:08:21 +0000

    changes SimpleNamespace to NamedTuple

--- aim/services.py
+++ aim/services.py
@@ -1,15 +1,39 @@
 from types import SimpleNamespace
+from typing import NamedTuple
 import os
 import sqlalchemy as sa
 
-S = SimpleNamespace()
-S.mysql_database = sa.engine.URL.create(
-    drivername="mysql+mysqldb",
-    username=os.environ["MARIADB_USER"],
-    password=os.environ["MARIADB_PASSWORD"],
-    host=os.environ["DATABASE_HOST"],
-    database=os.environ["MARIADB_DATABASE"],
+Services = NamedTuple(
+    "Services",
+    [
+        ("mysql_database", sa.engine.URL),
+        ("test_database", str),
+        ("ci_on", str | None),
+        ("digifeeds_api_url", str),
+    ],
 )
-S.test_database = "sqlite:///:memory:"
-S.ci_on = os.getenv("CI")
-S.digifeeds_api_url = os.getenv("DIGIFEEDS_API_URL") or "http://api:8000"
+
+S = Services(
+    mysql_database=sa.engine.URL.create(
+        drivername="mysql+mysqldb",
+        username=os.environ["MARIADB_USER"],
+        password=os.environ["MARIADB_PASSWORD"],
+        host=os.environ["DATABASE_HOST"],
+        database=os.environ["MARIADB_DATABASE"],
+    ),
+    test_database="sqlite:///:memory:",
+    ci_on=os.getenv("CI"),
+    digifeeds_api_url=os.getenv("DIGIFEEDS_API_URL") or "http://api:8000",
+)
+
+# S = SimpleNamespace()
+# S.mysql_database = sa.engine.URL.create(
+#     drivername="mysql+mysqldb",
+#     username=os.environ["MARIADB_USER"],
+#     password=os.environ["MARIADB_PASSWORD"],
+#     host=os.environ["DATABASE_HOST"],
+#     database=os.environ["MARIADB_DATABASE"],
+# )
+# S.test_database = "sqlite:///:memory:"
+# S.ci_on = os.getenv("CI")
+# S.digifeeds_api_url = os.getenv("DIGIFEEDS_API_URL") or "http://api:8000"
