47cf55dd46935fa857415d97b42ecdc05982d566
Monique Rio <mrio@umich.edu>
2024-09-25 15:54:30 +0000

    moves load_statuses to the aim cli

--- aim/cli/digifeeds.py
+++ aim/cli/digifeeds.py
@@ -1,4 +1,5 @@
 import typer
+from aim.digifeeds.database import models, main
 
 app = typer.Typer()
 
@@ -6,3 +7,9 @@ app = typer.Typer()
 @app.command()
 def add_to_db(barcode: str):
     print(f'Adding barcode "{barcode}" to database')
+
+
+@app.command()
+def load_statuses():
+    with main.SessionLocal() as db_session:
+        models.load_statuses(session=db_session)
