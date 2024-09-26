7a5fbb10052b459af96036f3f150fb6cb659b25f
Monique Rio <mrio@umich.edu>
2024-09-26 13:53:53 +0000

    adds add_item_status method to db_client

--- aim/digifeeds/db_client.py
+++ aim/digifeeds/db_client.py
@@ -3,9 +3,11 @@ from aim.services import S
 
 
 class DBClient:
+    def __init__(self) -> None:
+        self.base_url = S.digifeeds_api_url
+
     def get_item(self, barcode: str):
-        url = f"{S.digifeeds_api_url}/{barcode}"
-        print(url)
+        url = self._url(f"items/{barcode}")
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
@@ -15,8 +17,26 @@ class DBClient:
             response.raise_for_status()
 
     def add_item(self, barcode: str):
-        response = requests.post(f"{S.digifeeds_api_url}/{barcode}")
+        url = self._url(f"items/{barcode}")
+        response = requests.post(url)
         if response.status_code == 200:
             return response.json()
         else:
             response.raise_for_status()
+
+    def get_or_add_item(self, barcode: str):
+        item = self.get_item(barcode)
+        if not item:
+            item = self.add_item(barcode)
+        return item
+
+    def add_item_status(self, barcode: str, status: str):
+        url = self._url(f"items/{barcode}/status/{status}")
+        response = requests.put(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
+
+    def _url(self, path) -> str:
+        return f"{self.base_url}/{path}"
--- tests/digifeeds/test_db_client.py
+++ tests/digifeeds/test_db_client.py
@@ -7,7 +7,7 @@ from requests.exceptions import HTTPError
 
 @responses.activate
 def test_get_item_success():
-    url = f"{S.digifeeds_api_url}/my_barcode"
+    url = f"{S.digifeeds_api_url}/items/my_barcode"
     responses.get(url, json={"item": "my_item"}, status=200)
     item = DBClient().get_item(barcode="my_barcode")
     assert item == {"item": "my_item"}
@@ -15,7 +15,7 @@ def test_get_item_success():
 
 @responses.activate
 def test_get_item_not_found():
-    url = f"{S.digifeeds_api_url}/my_barcode"
+    url = f"{S.digifeeds_api_url}/items/my_barcode"
     responses.get(url, json={"item": "my_item"}, status=404)
     item = DBClient().get_item(barcode="my_barcode")
     assert item == None
@@ -23,7 +23,7 @@ def test_get_item_not_found():
 
 @responses.activate
 def test_get_item_raises_error():
-    url = f"{S.digifeeds_api_url}/my_barcode"
+    url = f"{S.digifeeds_api_url}/items/my_barcode"
     responses.get(url, json={"item": "my_item"}, status=500)
     with pytest.raises(Exception) as exc_info:
         DBClient().get_item(barcode="my_barcode")
@@ -32,7 +32,7 @@ def test_get_item_raises_error():
 
 @responses.activate
 def test_add_item_success():
-    url = f"{S.digifeeds_api_url}/my_barcode"
+    url = f"{S.digifeeds_api_url}/items/my_barcode"
     responses.post(url, json={"item": "my_item"}, status=200)
     item = DBClient().add_item(barcode="my_barcode")
     assert item == {"item": "my_item"}
@@ -40,8 +40,42 @@ def test_add_item_success():
 
 @responses.activate
 def test_add_item_failure():
-    url = f"{S.digifeeds_api_url}/my_barcode"
+    url = f"{S.digifeeds_api_url}/items/my_barcode"
     responses.post(url, json={"item": "my_item"}, status=404)
     with pytest.raises(Exception) as exc_info:
         DBClient().add_item(barcode="my_barcode")
+    assert exc_info.type is HTTPError
+
+
+@responses.activate
+def test_get_or_add_item_for_existing_item():
+    url = f"{S.digifeeds_api_url}/items/my_barcode"
+    responses.get(url, json={"item": "my_item"}, status=200)
+    item = DBClient().get_or_add_item(barcode="my_barcode")
+    assert item == {"item": "my_item"}
+
+
+@responses.activate
+def test_get_or_add_item_for_new_item():
+    url = f"{S.digifeeds_api_url}/items/my_barcode"
+    responses.get(url, json={}, status=404)
+    responses.post(url, json={"item": "my_item"}, status=200)
+    item = DBClient().get_or_add_item(barcode="my_barcode")
+    assert item == {"item": "my_item"}
+
+
+@responses.activate
+def test_add_item_status_success():
+    url = f"{S.digifeeds_api_url}/items/my_barcode/status/in_zephir"
+    responses.put(url, json={"item": "my_item"}, status=200)
+    item = DBClient().add_item_status(barcode="my_barcode", status="in_zephir")
+    assert item == {"item": "my_item"}
+
+
+@responses.activate
+def test_add_item_status_failure():
+    url = f"{S.digifeeds_api_url}/items/my_barcode/status/in_zephir"
+    responses.put(url, json={}, status=500)
+    with pytest.raises(Exception) as exc_info:
+        DBClient().add_item_status(barcode="my_barcode", status="in_zephir")
     assert exc_info.type is HTTPError
