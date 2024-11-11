f86348131e376cbf4b5508e4437015acc492c697
Monique Rio <mrio@umich.edu>
2024-11-11 19:44:38 +0000

    handles items in digifeeds set in alma that don't yet have a status in db

--- tests/digifeeds/test_add_to_db.py
+++ tests/digifeeds/test_add_to_db.py
@@ -24,6 +24,45 @@ def test_add_to_db_barcode_thats_in_the_digifeeds_set(mocker, item_data):
     assert result.barcode == "some_barcode"
 
 
+@responses.activate
+def test_add_to_db_barcode_thats_in_the_digifeeds_set_but_doesnt_have_status(
+    mocker, item_data
+):
+    item_data["statuses"][0]["name"] = "some_other_status"
+    get_item_mock = mocker.patch(
+        "aim.digifeeds.db_client.DBClient.get_or_add_item", return_value=item_data
+    )
+    add_status_mock = mocker.patch(
+        "aim.digifeeds.db_client.DBClient.add_item_status", return_value=item_data
+    )
+    error_body = {
+        "errorsExist": True,
+        "errorList": {
+            "error": [
+                {
+                    "errorCode": "60115",
+                    "errorMessage": "The following ID(s) are already in the set ['some_barcode']",
+                    "trackingId": "E01-2609211329-8EKLP-AWAE1781893571",
+                }
+            ]
+        },
+    }
+    add_to_digifeeds_url = f"{S.alma_api_url}/conf/sets/{S.digifeeds_set_id}"
+    add_to_digifeeds_set = responses.post(
+        add_to_digifeeds_url,
+        json=error_body,
+        status=400,
+    )
+
+    result = add_to_db("some_barcode")
+    get_item_mock.assert_called_once()
+    add_status_mock.assert_called_once_with(
+        barcode="some_barcode", status="added_to_digifeeds_set"
+    )
+    assert add_to_digifeeds_set.call_count == 1
+    assert result.barcode == "some_barcode"
+
+
 def test_add_to_db_barcode_thats_not_in_the_digifeeds_set(mocker, item_data):
     item_data["statuses"][0]["name"] = "some_other_status"
     get_item_mock = mocker.patch(
